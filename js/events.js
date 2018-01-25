03d6b5bf62d429e609334ef0c14a36a9c31ae828
Nic Travis <nptravis@yahoo.com>
2018-01-25 09:01:45 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
 //define functions here
 
-$(document).ready(function(){
+function getIt(){
+  $('p').on('click', function(e){
+    alert("Hey!");
+  });
+}
 
+function frameIt(){
+  $('img').addClass('tasty');
+}
+
+function pressIt(){
+  $('input').on('keydown', function(e){
+    if (e.which === 71){
+      alert("You have pressed G!");
+    }
+  })
+}
+
+function submitIt(){
+  $('form').on('submit', function(e){
+    alert("Your form is going to be submitted now.");
+  })
+}
+
+$(document).ready(function(){
+getIt();
+frameIt();
+pressIt();
+submitIt();
 // call functions here
 
 });
