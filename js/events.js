74f0864fa0079f1bbeb757b91991d558e36f07e9
Nic Travis <nptravis@yahoo.com>
2018-01-25 08:44:42 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -17,6 +17,11 @@ function pressIt(){
     }
   })
 }
+function submitIt(){
+  $('form').on('submit', function(e){
+    alert("Your form is going to be submitted now.")
+  })
+}
 $(document).ready(function(){
 
 // call functions here
